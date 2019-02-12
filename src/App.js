a10ca705ff0401598ad275f14ffcd64e7b90c80d
Michael Lai <mike24567@gmail.com>
2019-02-12 10:47:22 -0800

    checking netlify

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Michael Lai is awesome
+            Michael Lai is awesome!!!!
           </a>
         </header>
       </div>
